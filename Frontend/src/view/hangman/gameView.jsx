1f31351fc9fad66cefbc3969094549edcd3b6d64
raihan678 <akunnrandom337@gmail.com>
2026-09-10 19:14:53 +0700

    1.8.2 tambah navigate

--- Frontend/src/view/hangman/gameView.jsx
+++ Frontend/src/view/hangman/gameView.jsx
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react'
 import Swal from 'sweetalert2'
-import {navigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 
 const navigate = useNavigate()
 
